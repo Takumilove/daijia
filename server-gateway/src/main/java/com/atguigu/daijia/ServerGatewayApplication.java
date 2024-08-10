8256a4886ab0b2b6909cb815741e6656a7c84f09
Takumilove <raopengfei55@gmail.com>
2024-08-10 19:09:41 +0800

    1.1

--- server-gateway/src/main/java/com/atguigu/daijia/ServerGatewayApplication.java
+++ server-gateway/src/main/java/com/atguigu/daijia/ServerGatewayApplication.java
@@ -10,8 +10,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class ServerGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGatewayApplication.class, args);
+    }
 
 }
