2e00c476e7989cdcc183ad7fd178a6bb3882a346
Takumilove <raopengfei55@gmail.com>
2024-08-11 08:22:06 +0800

    1.1

--- web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerServiceImpl.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerServiceImpl.java
@@ -1,9 +1,18 @@
 package com.atguigu.daijia.customer.service.impl;
 
+import com.atguigu.daijia.common.constant.RedisConstant;
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
+import com.atguigu.daijia.customer.client.CustomerInfoFeignClient;
 import com.atguigu.daijia.customer.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -11,9 +20,35 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
 
+    // 注入远程调用接口
+    private final CustomerInfoFeignClient client;
+    private final RedisTemplate redisTemplate;
 
     @Override
     public String login(String code) {
-        return null;
+        // 1.远程调用，返回用户id
+        Result<Long> loginResult = client.login(code);
+
+        // 2.判断如果返回失败，返回错误提示
+        Integer codeResult = loginResult.getCode();
+        if (codeResult != 200) {
+            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+        }
+        // 3.获取远程调用返回的用户id
+        Long customerId = loginResult.getData();
+        // 4.判断返回用户id是否为空，如果为空，返回错误提示
+        if (customerId == null) {
+            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+        }
+        // 5.生成token字符串
+        String token = UUID.randomUUID().toString().replaceAll("-", "");
+        // 6.把用户id放到Redis，设置过期时间
+        // key:token value:customerId
+        redisTemplate.opsForValue().set(RedisConstant.USER_LOGIN_KEY_PREFIX + token,
+                                        customerId.toString(),
+                                        RedisConstant.USER_LOGIN_KEY_TIMEOUT,
+                                        TimeUnit.SECONDS);
+        // 7.返回token字符串
+        return token;
     }
 }
