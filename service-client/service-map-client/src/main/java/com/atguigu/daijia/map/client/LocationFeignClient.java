d18ee08f7d9bcd769a33b3706114e35c727220e5
Takumilove <raopengfei55@gmail.com>
2024-08-19 11:40:00 +0800

    1.5

--- service-client/service-map-client/src/main/java/com/atguigu/daijia/map/client/LocationFeignClient.java
+++ service-client/service-map-client/src/main/java/com/atguigu/daijia/map/client/LocationFeignClient.java
@@ -1,12 +1,16 @@
 package com.atguigu.daijia.map.client;
 
 import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.model.form.map.SearchNearByDriverForm;
 import com.atguigu.daijia.model.form.map.UpdateDriverLocationForm;
+import com.atguigu.daijia.model.vo.map.NearByDriverVo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient(value = "service-map")
 public interface LocationFeignClient {
@@ -27,4 +31,8 @@ public interface LocationFeignClient {
      */
     @DeleteMapping("/map/location/removeDriverLocation/{driverId}")
     Result<Boolean> removeDriverLocation(@PathVariable("driverId") Long driverId);
+
+    // 搜索附近满足条件的司机
+    @PostMapping("/map/location/searchNearbyDriver")
+    Result<List<NearByDriverVo>> searchNearByDriver(@RequestBody SearchNearByDriverForm searchNearByDriverForm);
 }
